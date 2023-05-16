57226d1d95a9ced5e7c348a275284e02a89fe20d
placeparks <86215469+placeparks@users.noreply.github.com>
2023-05-16 03:05:40 -0700

    Update contractAddresses.js

--- consts/contractAddresses.js
+++ consts/contractAddresses.js
@@ -1,5 +1,6 @@
 export const editionDropContractAddress =
-  "0xF71C561c223A1549568e0301D9897Bd504317717"
-export const tokenContractAddress = "0xB490Fd5CE7B4fD74bCA4368fb113d6AA4AED1225"
+  "0x8FABe7FeF82dEe91123442f3d08E9f7282eF0679"
+export const tokenContractAddress = "0xD6263eB134Abbc49f137d0D64585262B301606EF"
 export const stakingContractAddress =
-  "0x605A7b08228B1dcc039251230198E9a68b833F67"
+  "0xb96cf3a10464BdD6C981F4c5A13989723D4eA3D0"
+
